f8677a07e8a0278b477c9856b2865e1576006213
elhanan1981 <brachivahav@gmail.com>
2024-05-12 14:33:23 +0300

    refactor add a num to floors ,buildings and elevators in settings class

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,13 @@
 import { Building } from './building';
 import { Factory } from './factory';
+import { Settings } from './settings';
 
 function main() {
   let buildings: Building[] = [];
-  for (let i = 0; i < 20; i++) {
-    buildings.push(Factory.createBuilding(3, 20));
+  for (let i = 0; i < Settings.amountBuildings; i++) {
+    buildings.push(
+      Factory.createBuilding(Settings.amountElevators, Settings.amountFloors),
+    );
   }
 }
 
